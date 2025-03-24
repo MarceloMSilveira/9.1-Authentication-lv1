ce9f3f89bfabd4b2ce96e5ae9bfc2c25b7de858e
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2025-03-24 10:04:42 -0300

    init

---
+++ index.js
@@ -0,0 +1,77 @@
+import express from "express";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import 'dotenv/config';
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+const {Client} = pg;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(morgan('dev'));
+
+const db = new Client ({
+  user: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  password: process.env.db_pwd,
+  database: 'secrets'
+});
+
+await db.connect();
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+app.post("/register", async (req, res) => {
+  const {username:email,password} = req.body;
+  try {
+    const checkEmail = await db.query("SELECT * FROM users WHERE email=$1",[email]);
+    if (checkEmail.rows.length>0) {
+      return res.send("Email already exists. Try logging in.");
+    }    
+    const query = "INSERT INTO users (email,password) VALUES ($1,$2) RETURNING id";
+    const result = await db.query(query,[email,password]);
+    console.log(result.rows[0].id);
+    res.render("secrets.ejs");  
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const {username:email,password} = req.body;
+  try {
+    //check if email exists:
+    const result = await db.query("SELECT password FROM users WHERE email=$1;",[email]);
+    if (result.rows.length===0) {
+      return res.send("Your email is not register yet. Try to retister first.")
+    }
+    const passwordInDB = result.rows[0].password;
+    if (password===passwordInDB){
+      return res.status(200).render("secrets.ejs");
+    } else {
+      return res.status(401).send("incorret password.");
+    }
+  } catch (error) {
+    console.log(error)
+    res.status(500).send(error.details)
+  }
+  res.render("home.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
